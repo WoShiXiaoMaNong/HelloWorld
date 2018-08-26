f5dbd26a092cdf0e79d2878c07eb6cd483744462
woshixiaomanong <zhongming139@126.com>
2018-08-26 21:52:00 +0800

    test git

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.gongzong.common.JDBCUtil;
+import com.gongzong.common.JDBCUtil;  
 
 @RunWith(SpringRunner.class)
 public class DemoApplicationTests {
